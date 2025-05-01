27b060ef29c121bcf78db4cb86868a97e1df316a
ueno <ueno@pwcom.co.jp>
2025-05-01 10:52:24 +0900

    キャラクターDBページに戻るボタン追加

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Characters from './Characters';
 function App() {
   return (
     <div className="App">
+      <div className="AppBackButton"><a href="https://dndhideout.com/">DnD Hideout</a></div>
+      
       <Characters/>
     </div>
   );
